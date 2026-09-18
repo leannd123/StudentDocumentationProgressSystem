f26175a11aae82be2c89f79f311e4e3bb4431d58
leannd123 <lmj64879@email.vccs.edu>
2026-09-17 21:11:05 -0400

    Add initial Java application

---
+++ StudentDocumentationApp.java
@@ -0,0 +1,16 @@
+
+/**
+ * Student Documentation & Progress System
+ *
+ * @author LeAnn Johnson
+ * @version 1.0
+ */
+public class StudentDocumentationApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Project: Student Documentation & Progress System");
+        System.out.println("Status: Initial project structure created successfully.");
+        System.out.println("Current Stage: Milestone 1 - Project Foundation");
+    }
+}
